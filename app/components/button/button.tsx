80bf5da4f355f6654666307c64da1f53250677cb
Thanh Bui <thanh.v.bui511@gmail.com>
2024-09-25 22:20:51 +0300

    update next, fix font Cartilo not found

--- app/components/button/button.tsx
+++ app/components/button/button.tsx
@@ -1,11 +1,11 @@
 "use client"
-import {Carlito} from "next/font/google";
+import {Poppins} from "next/font/google";
 import './button.scss';
 
-const carlito = Carlito({
+const poppins = Poppins({
 	weight: '400',
 	subsets: ['latin']
 })
 export default function MyButton() {
-	return <button className={`${carlito.className} rounded-full text-background bg-content`}>Hire me</button>
+	return <button className={`${poppins.className} rounded-full text-background bg-content`}>Hire me</button>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {Carlito, Radley} from "next/font/google";
+import {Poppins, Radley} from "next/font/google";
 import './home.scss'
 import MyButton from "@/app/components/button/button";
 
@@ -7,7 +7,7 @@ const radley = Radley({
 	subsets: ['latin'],
 })
 
-const carlito = Carlito({
+const poppins = Poppins({
 	weight: '400',
 	subsets: ['latin']
 })
@@ -17,7 +17,7 @@ export default function Home() {
 		<main className="flex min-h-screen flex-col items-center justify-center p-24 bg-background">
 			<div className="flex flex-col flex-item-container justify-center">
 				<h1 className={`${radley.className} text-black`}><i>Hello</i>, <span className="text-content">I am Thanh</span></h1>
-				<p className={`${carlito.className} text-black`}>Code is love, code is live</p>
+				<p className={`${poppins.className} text-black`}>Code is love, code is live</p>
 			</div>
 			<MyButton/>
 		</main>
